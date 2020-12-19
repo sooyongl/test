6813bf3d62b1aa04abadd868d70d0a0ea5eeda3e
Sooyong Lee <pagass103@utexas.edu>
2020-12-19 16:00:24 -0600

    Update list_of_addins.r

--- list_of_addins.r
+++ list_of_addins.r
@@ -4,4 +4,8 @@
 ## Short cut : ctrl + shift + A with block
 library(styler)
 library(prettycode)
-
+# or
+library(formatR)
+# tidy_eval("file path")
+# tidy_eval("C:/Users/PC_name/Desktop/myfile.R")
+usage(glm, width = 40) # args(glm)
