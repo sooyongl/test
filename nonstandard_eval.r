71686ed0d5346a6535980a94ae9a38b0845b38da
Sooyong Lee <pagass103@utexas.edu>
2021-01-13 14:15:03 -0600

    Create nonstandard_eval.r

---
+++ nonstandard_eval.r
@@ -0,0 +1,12 @@
+# substitute() = non-standard evaluation possible
+# return an expression (return whatever input)
+# paired with deparse() - turns expressions into characters
+# quote() - similar to substitute() but return always its argument only (not interactive with inputs)
+# eval()
+
+# as.name() converts a string to a name = as.symbole (is.name(), is.symbol())
+# quote() captures names. 
+# paired with parse(text = "")
+
+# Call
+# A call is similar to a list. It has length, and indexing. 
